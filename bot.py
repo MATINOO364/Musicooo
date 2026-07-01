9338d9002e95071788f5d84dd5ff1ffef12bdf3a
MATINOO364 <mohammedzadematin@gmail.com>
2026-07-01 14:44:50 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,18 @@
-from telegram import Update, ReplyKeyboardMarkup
+from telegram import (
+    Update,
+    ReplyKeyboardMarkup,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup
+)
+
 from telegram.ext import *
 from tinydb import TinyDB
 
-from config import BOT_TOKEN, CHANNEL_ID
+from config import BOT_TOKEN
 
 
 db = TinyDB("db.json")
+
 
 
 # ذخیره آهنگ کانال
@@ -16,9 +23,11 @@ async def channel_music(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not msg:
         return
 
+
     if msg.audio or msg.document:
 
         number = len(db.all()) + 1
+
 
         if msg.audio:
             file_id = msg.audio.file_id
@@ -29,25 +38,30 @@ async def channel_music(update: Update, context: ContextTypes.DEFAULT_TYPE):
             name = msg.document.file_name
 
 
+
         db.insert({
             "id": number,
             "name": name,
             "file_id": file_id
         })
 
+
         print("Saved:", name)
 
 
 
-# start
+
+# شروع
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     keyboard = [
-        ["🎵 آخرین موزیک ها"]
+        ["🎵 آخرین موزیک ها"],
+        ["🔎 جستجو"]
     ]
 
+
     await update.message.reply_text(
-        "سلام 👋",
+        "سلام 👋\nآهنگتو پیدا کن",
         reply_markup=ReplyKeyboardMarkup(
             keyboard,
             resize_keyboard=True
@@ -56,49 +70,117 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 
+
+
 # آخرین آهنگ ها
 async def latest(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     songs = db.all()
 
+
     if not songs:
+
         await update.message.reply_text(
-            "هیچ آهنگی ذخیره نشده ❌"
+            "هیچ آهنگی نداریم ❌"
         )
+
         return
 
 
-    text = "🎵 آخرین موزیک ها:\n\n"
+
+    buttons = []
 
 
-    for s in songs[-10:]:
-        text += f"{s['id']} - {s['name']}\n"
+    for song in songs[-10:]:
+
+        buttons.append([
+
+            InlineKeyboardButton(
+                f"🎵 {song['name']}",
+                callback_data=str(song["id"])
+            )
+
+        ])
 
 
-    await update.message.reply_text(text)
+
+    await update.message.reply_text(
+
+        "آخرین موزیک‌ها:",
+
+        reply_markup=InlineKeyboardMarkup(buttons)
+
+    )
 
 
 
-# ارسال با کد
-async def search(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
-    text = update.message.text
+
+# ارسال آهنگ با دکمه
+async def send_song(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    query = update.callback_query
+
+    await query.answer()
+
+
+
+    song_id = int(query.data)
+
+
 
     for song in db.all():
 
+
+        if song["id"] == song_id:
+
+
+            await query.message.reply_audio(
+
+                audio=song["file_id"],
+
+                caption=song["name"]
+
+            )
+
+
+            return
+
+
+
+
+
+# سرچ با کد
+async def search(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    text = update.message.text.strip()
+
+
+
+    for song in db.all():
+
+
         if str(song["id"]) == text:
 
+
             await update.message.reply_audio(
-                song["file_id"],
+
+                audio=song["file_id"],
+
                 caption=song["name"]
+
             )
 
             return
 
 
+
+
     await update.message.reply_text(
-        "پیدا نشد ❌"
+        "❌ پیدا نشد"
     )
+
+
 
 
 
@@ -106,33 +188,79 @@ async def search(update: Update, context: ContextTypes.DEFAULT_TYPE):
 app = Application.builder().token(BOT_TOKEN).build()
 
 
-app.add_handler(CommandHandler("start", start))
 
-
+# start
 app.add_handler(
+    CommandHandler(
+        "start",
+        start
+    )
+)
+
+
+
+# کانال
+app.add_handler(
+
     MessageHandler(
+
         filters.UpdateType.CHANNEL_POST,
+
         channel_music
+
     )
+
 )
 
 
+
+# دکمه آخرین موزیک
+
 app.add_handler(
+
     MessageHandler(
+
         filters.Regex("^🎵 آخرین موزیک ها$"),
+
         latest
+
     )
+
 )
 
+
+
+# کلیک روی آهنگ
 
 app.add_handler(
-    MessageHandler(
-        filters.TEXT,
-        search
+
+    CallbackQueryHandler(
+
+        send_song
+
     )
+
 )
 
 
-print("RUNNING")
+
+# پیام معمولی
+
+app.add_handler(
+
+    MessageHandler(
+
+        filters.TEXT,
+
+        search
+
+    )
+
+)
+
+
+
+print("BOT RUNNING")
+
 
 app.run_polling()
