5f2ba1eeca4f9c2a07795831d4652535b3225ae9
MATINOO364 <mohammedzadematin@gmail.com>
2026-07-01 15:03:18 +0330

    Update database.py

--- database.py
+++ database.py
@@ -1,82 +1,43 @@
 from tinydb import TinyDB, Query
 
-
 db = TinyDB("db.json")
-
 
 songs = db.table("songs")
 users = db.table("users")
 
 
-
-def add_song(data):
-
-    songs.insert(data)
-
+def add_song(song):
+    songs.insert(song)
 
 
 def all_songs():
-
     return songs.all()
 
 
-
-def get_next_id():
-
-    if not songs.all():
-        return 1
-
+def next_code():
     return len(songs.all()) + 1
 
 
-
-def find_song(code):
-
+def find_by_code(code):
     Q = Query()
-
-    result = songs.search(
-        Q.code == int(code)
-    )
-
-    if result:
-        return result[0]
-
-    return None
+    res = songs.search(Q.code == code)
+    return res[0] if res else None
 
 
-
-def search_name(name):
-
+def search_by_name(text):
     Q = Query()
-
-    result = songs.search(
-        Q.name.test(
-            lambda x: name.lower() in x.lower()
-        )
-    )
-
-    return result
-
+    return songs.search(Q.name.test(lambda x: text.lower() in x.lower()))
 
 
 def add_user(uid):
-
     Q = Query()
-
     if not users.search(Q.id == uid):
-
-        users.insert({
-            "id":uid
-        })
-
+        users.insert({"id": uid})
 
 
 def users_count():
-
     return len(users.all())
 
 
-
 def songs_count():
-
     return len(songs.all())
